c3e82af6ce711b317b5a7db6b30e9fac368b1926
Velial <Velialxxx69@gmail.com>
2022-10-22 00:18:36 +0300

    Made a method for viewing calls

--- src/OopPhone/Phone.java
+++ src/OopPhone/Phone.java
@@ -1,13 +1,30 @@
 package OopPhone;
 
 public class Phone {
-    String seriesNumber;
+    String number;
     String model;
-    String core;
     double weight;
-    double displaySize;
 
-    double getVolume() {
-        return weight * displaySize;
+    Phone(String n, String m, double w) {
+        number = n;
+        model = m;
+        weight = w;
+    }
+
+    Phone(String n, String m) {
+        number = n;
+        model = m;
+    }
+
+    Phone(){
+
+    }
+
+    void receiveCall(String name) {
+        System.out.println("Calling " + name);
+    }
+
+    String getNumber() {
+        return number;
     }
 }
--- src/OopPhone/PhoneMain.java
+++ src/OopPhone/PhoneMain.java
@@ -3,14 +3,26 @@ package OopPhone;
 public class PhoneMain {
     public static void main(String[] args) {
 
-        Phone myPhone1  = new Phone();
-        Phone myPhone2 = new Phone();
+        Phone myPhone1 = new Phone("00231248", "Apple", 173);
+        Phone myPhone2 = new Phone("00361239", "Samsung");
+        Phone myPhone3 = new Phone();
 
-        myPhone1.displaySize = 6.5;
-        myPhone1.model = "Apple";
-        myPhone1.seriesNumber = "201432123";
-        myPhone1.core = "4";
-        myPhone1.weight = 100.50;
+        System.out.println(myPhone1.getNumber());
+        System.out.println(myPhone1.model);
+        System.out.println(myPhone1.weight);
+        System.out.println();
 
+        System.out.println(myPhone2.getNumber());
+        System.out.println(myPhone2.model);
+        System.out.println(myPhone2.weight);
+        System.out.println();
+
+        System.out.println(myPhone3.getNumber());
+        System.out.println(myPhone3.model);
+        System.out.println(myPhone3.weight);
+        System.out.println();
+
+        myPhone1.receiveCall("Mum");
+        myPhone2.receiveCall("Dad");
     }
 }
