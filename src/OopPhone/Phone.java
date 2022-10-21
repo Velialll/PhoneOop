452091884b1ed3347e357ebb5f60e2a628e48bba
Velial <Velialxxx69@gmail.com>
2022-10-22 01:35:13 +0300

    Learned the keyword "this" and found out about the generator

--- src/OopPhone/Phone.java
+++ src/OopPhone/Phone.java
@@ -5,13 +5,23 @@ public class Phone {
     String model;
     double weight;
 
+    /**
+     * Ключевое слово "this" должно стоять первым
+     * Вызывает метод number и model с второго конструктора
+     * Что бы избежать повторения
+
+     * Комбинация клавиш "ALT" + "Insert"
+     * Вызывает меню "Generate" в котором можно
+     * Сгинерировать конструктор выбирая нужые параметры
+     * Для данного конструктора (зажать "CTRL")
+     */
     Phone(String number, String model, double weight) {
-        this.number = number;
-        this.model = model;
+        this(number,model);
         this.weight = weight;
     }
 
     Phone(String number, String model) {
+        this();
         this.number = number;
         this.model = model;
     }
